9533ae8ccc63f9b6eb49807e0838deaed39357d4
William Roger <williamroger.frontend@gmail.com>
2023-03-25 18:31:29 -0300

    adicionado componente Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
 import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar';
 
 function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="William Roger" 
-        content="Conteúdo do meu post maneiro." 
-      />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        
+        <main>
+          <Post
+            author="William Roger"
+            content="Conteúdo do meu post maneiro."
+          />
+          <Post
+            author="William Roger"
+            content="Conteúdo do meu post maneiro."
+          />
+        </main>
+      </div>
     </div>
   )
 }
