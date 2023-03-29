4511dfdad71bad5dcd3a8c97aa3a9e62f6816746
William Roger <williamroger.frontend@gmail.com>
2023-03-29 01:24:03 -0300

    adicionado novo componente Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,34 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/williamroger.png" />
+          <div className={styles.authorInfo}>
+            <strong>William Roger</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title='28 de Março de 2023 às 01:00h' dateTime='2023-03-28 01:00:00'>
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 </p>
+
+        <p><a href='#'>jane.design/doctorcare</a></p>
+
+        <p>
+          <a href='#'>#novoprojeto</a>{' '}
+          <a href='#'>#nlw</a>{' '}
+          <a href='#'>#rocketseat</a>{' '}
+        </p>
+      </div>
+    </article>
+  );
+}
