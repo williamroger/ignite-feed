54ca045bef811aae8346a5bfdad2a4434baddfd4
William Roger <williamroger.frontend@gmail.com>
2023-05-23 18:53:55 -0300

    comunicação entre os componentes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -31,6 +31,10 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value);
   }
 
+  function deleteComment(comment) {
+    console.log(`deletar o comentário ${comment}`);
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -72,7 +76,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment key={comment} content={comment} />;
+          return (
+            <Comment 
+              key={comment} 
+              content={comment} 
+              onDeleteComment={deleteComment} 
+            />
+          );
         })}
       </div>
     </article>
