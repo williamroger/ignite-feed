53c98ba5ff60ead8e20ab9aa3c680d6aeef6a251
William Roger <williamroger.frontend@gmail.com>
2023-05-23 00:45:11 -0300

    adicionado propriedade key

--- src/App.jsx
+++ src/App.jsx
@@ -66,6 +66,7 @@ function App() {
           {posts.map(post => {
             return (
               <Post 
+                key={post.id}
                 author={post.author}
                 content={post.content}
                 publishedAt={post.publishedAt}
