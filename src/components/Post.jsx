e048f274033b84db51655f4345a1fe750b8e577b
William Roger <williamroger.frontend@gmail.com>
2023-05-24 01:50:58 -0300

    removendo comentários com imutabilidade

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -31,8 +31,12 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value);
   }
 
-  function deleteComment(comment) {
-    console.log(`deletar o comentário ${comment}`);
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter(comment => {
+      return comment !== commentToDelete;
+    });
+
+    setComments(commentsWithoutDeletedOne);
   }
 
   return (
