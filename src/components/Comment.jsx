acd9b45ab7cd1a9fc4208fcb4183fa80ed328741
William Roger <williamroger.frontend@gmail.com>
2023-05-24 18:20:08 -0300

    adicionada funcionalidade de likes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,10 +2,17 @@ import { ThumbsUp, Trash } from '@phosphor-icons/react';
 import styles from './Comment.module.css';
 
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -34,9 +41,9 @@ export function Comment({ content, onDeleteComment }) {
           </p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp size={20}/>
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button> 
         </footer>
       </div>
